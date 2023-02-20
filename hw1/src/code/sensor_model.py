a24868d978be8a8e2be4fd65aa1f8f0092f76248
bharath somayajula <thebharathsk@gmail.com>
2023-02-20 10:28:00 -0500

    vectorize ray casting and make it a new module

--- hw1/src/code/sensor_model.py
+++ hw1/src/code/sensor_model.py
@@ -13,7 +13,7 @@ from matplotlib import pyplot as plt
 from scipy.stats import norm
 
 from map_reader import MapReader
-
+import ray_casting as rc
 
 class SensorModel:
     """
@@ -35,193 +35,12 @@ class SensorModel:
 
         # Used in p_max and p_rand, optionally in ray casting
         self._max_range = 1000
-
-        # Used for thresholding obstacles of the occupancy map
-        self._min_probability = 0.35
-
-        # Used in sampling angles in ray casting
-        self._subsampling = 5
         
-        # Number of processes to be used for subsampling
-        self.num_processes = 2
-        
-        # Store oocupancy map
-        self.map = occupancy_map
-        
-        # Dimensions of occupancy map
-        self.h, self.w = self.map._occupancy_map.shape
-        
-        # Distance that ray can skip during casting iterations
-        self.ray_skip_dist = 10
-        
-        #separation between laser and robot center in cm
-        self.laser_loc = 25
-
         #variable to ensure non-zero probabilities
         self.eps = 1e-5
         
-        # Store rays
-        self.rays = self.map._occupancy_map.copy()
-        
-    def sensor_location(self, x):
-        """Find sensor's position based on state of robot
-
-        Args:
-            x (list): state of robot represented by a particle
-        """
-        #get x, y, theta of robot from state
-        rx, ry, theta = x
-        
-        #compute laser's location
-        lx = rx + self.laser_loc*np.cos(theta) 
-        ly = ry + self.laser_loc*np.sin(theta)
-        
-        return [lx, ly, theta]
-    
-    def ray_casting_vectorized(self, arg_list, results):
-        """vectorized implementation of ray casting
-
-        Args:
-            args (Queue): Queue with input arguments
-            results (Queue): Queue that stores results
-        """
-        # while True:
-        #     #sample and input
-        #     try:
-        #         x, angle = args.get_nowait()
-        #     except queue.Empty:
-        #         break
-        #     #if sampling is successful, perform ray casting
-        #     else:
-        #         #cast a ray
-        #         z_gt_a = self.ray_casting(x, angle)
-        #         results.put([angle, z_gt_a])
-
-        #iteratively process inputs and cast rays
-        for a in arg_list:
-            x, angle = a
-            z_gt_a = self.ray_casting(x, angle)
-            results.put([a, z_gt_a])
-         
-    def ray_casting(self, x, angle):
-        """ray casting algorithm to find true range 
-
-        Args:
-            x (list): state of the range sensor represented by a particle
-            angle (float): angle of laser beam
-        """
-        
-        #unpack particle
-        lx, ly, ltheta = x
-        
-        #measure angle of laser beam in global reference frame
-        theta = (ltheta + angle) - math.pi/2
-        
-        #perform ray casting
-        #initialize positions to positions of range sensor
-        cx, cy = lx, ly
-        
-        #iterate till wall is hit or the ray reaches edge of map
-        while True:
-            #convert cm into px
-            cx_p = math.floor(cx/self.map._resolution)
-            cy_p = math.floor(cy/self.map._resolution)
-            
-            #if ray goes beyond edge
-            if cx_p < 0 or cx_p > self.w - 1 or \
-                cy_p < 0 or cy_p > self.h - 1:
-                    break
-            
-            #if ray hits the wall
-            if self.map._occupancy_map[cy_p][cx_p] > self._min_probability \
-                or self.map._occupancy_map[cy_p][cx_p] == -1:
-                break
-            
-            #update x and y coordinates
-            cx = cx + self.ray_skip_dist*np.cos(theta)
-            cy = cy + self.ray_skip_dist*np.sin(theta)
-            
-            self.rays[cy_p][cx_p] = -2
-            #print("ray = ", cy, cx, self.rays[cy_p][cx_p])
-            
-        z_gt = math.sqrt((cx-lx)**2 + (cy-ly)**2)
-
-        return z_gt
-    
-    def get_true_ranges(self, x):
-        """function to find true range for a given state(x,y,theta) of robot
-
-        Args:
-            x (list): state of range sensor
-        """
-        #array to store true ranges
-        z_gt = []
-        
-        angles = np.arange(1, 181, self._subsampling)
-        print('num angles = ', angles.size)
-        
-        #iterate based on subsampling
-        for i in angles:
-            #compute range using ray casting
-            z_gt_angle = self.ray_casting(x, i*(math.pi/180))
-                        
-            #add range to list
-            z_gt.append(z_gt_angle)
-        
-        return z_gt
-    
-    def get_true_ranges_vectorized(self, x):
-        """function to find true range for a given state(x,y,theta) of robot using vectorized implementation
-
-        Args:
-            x (list): state of range sensor
-        """
-        #array to store true ranges
-        z_gt = []
-        
-        #store angles at which rays are cast
-        angles = np.arange(1, 181, self._subsampling)
-        
-        #create list to store input arguments
-        args = []
-        for a in angles:
-            args.append([x, a*(math.pi/180)])
-        
-        #create Queue to store results
-        results = Queue()
-        
-        #split inputs into chunks
-        chunk_size = len(args)//self.num_processes
-        args_list = [args[i:i+chunk_size] for i in range(0, len(args), chunk_size)]
-        
-        #start processes
-        t1 = time.time()
-        
-        processes = []
-        for a in args_list:
-            p = Process(target = self.ray_casting_vectorized, args=(a,results,))
-            processes.append(p)
-            p.start()
-            print('Started process', len(a))
-
-        t2 = time.time()
-        
-        #end processes
-        for p in processes:
-            p.join()
-        t3 = time.time()
-        
-        print("in time = ", t2-t1, t3-t2)
-        # #unpack results
-        # while not results.empty():
-        #     results.get()
-        #print(results)
-        # z_gt = [0 for i in range(angles.size)]
-        
-        # while not results.empty():
-        #     a, z = results.get()
-            
-        # return z_gt
+        #initialize ray casting object
+        self.ray_casting = rc.RayCasting(occupancy_map)
     
     def sensor_probs(self, z_t, z_gt):
         """function to compute probabiltiies of measurement
@@ -286,7 +105,14 @@ class SensorModel:
         p4[mask] = 1/(self._max_range)
                 
         return p1, p2, p3, p4
-        
+    
+    def get_true_ranges(self, x_t):
+        """Find true ranges
+
+        Args:
+            x_t (list): state of robot
+        """
+        return self.ray_casting.get_true_ranges(x_t)
 
     def beam_range_finder_model(self, z_t1_arr, x_t1):
         """
@@ -298,15 +124,12 @@ class SensorModel:
         TODO : Add your code here
         """
         prob_zt1 = 1.0
-        
-        #find laser position based on robot position
-        x_sensor = self.sensor_location(x_t1)
-        
+                
         #perform ray casting to find GT ranges at various angles
-        z_gt = self.get_true_ranges(x_sensor)
+        z_gt = self.ray_casting.get_true_ranges(x_t1)
         
         #sample laser measurements
-        z_t1_arr = z_t1_arr[::self._subsampling]
+        z_t1_arr = z_t1_arr[::self.ray_casting._subsampling]
         
         #find probabilities
         p1, p2, p3, p4 = self.sensor_probs(z_t1_arr, z_gt)
@@ -333,13 +156,18 @@ if __name__ == "__main__":
     sm = SensorModel(map1)
     
     t1 = time.time()
+    
     xx = 4110
     yy = 5130
-    for n in range(1):
+    
+    for n in range(500):
         z_gt_1 = sm.get_true_ranges([xx, yy, math.pi/2])
-        t2 = time.time()
+    
+    t2 = time.time()
         
-    print("Normal time = ", t2-t1)
+    print("Vectorized time = ", t2-t1)
+    
+    print(z_gt_1)
     
     # #sm.rays[yy//10, xx//10] = -5
     # t3 = time.time()
@@ -354,7 +182,6 @@ if __name__ == "__main__":
     # print("Vectorized time = ", t4-t3)
     
     #plt.imshow(sm.map._occupancy_map, 'gray')
-    plt.imshow(sm.rays, 'gray')
-    plt.show()
+    #plt.imshow(sm.rays, 'gray')
     #plt.savefig('./rays.png')
     

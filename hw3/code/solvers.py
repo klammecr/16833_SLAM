a656e4229fd9641ed4c45ea84fa482cd09f9e242
klammecr <christopher.klammer@gmail.com>
2023-03-30 17:47:33 -0400

    Solvers

--- hw3/code/solvers.py
+++ hw3/code/solvers.py
@@ -17,33 +17,45 @@ def solve_default(A, b):
 
 
 def solve_pinv(A, b):
-    # TODO: return x s.t. Ax = b using pseudo inverse.
+    # return x s.t. Ax = b using pseudo inverse.
     N = A.shape[1]
-    x = np.zeros((N, ))
+    hessian = A.T @ A # Or information matrix (inverse of the covariance matrix)
+    x = inv(hessian) @ A.T @ b
     return x, None
 
 
 def solve_lu(A, b):
-    # TODO: return x, U s.t. Ax = b, and A = LU with LU decomposition.
+    # return x, U s.t. Ax = b, and A = LU with LU decomposition.
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.splu.html
-    N = A.shape[1]
-    x = np.zeros((N, ))
-    U = eye(N)
+
+    # Factorize A into L and U
+    inv_A = splu(A, perm_spec = "NATURAL")
+    hessian = A.T @ A
+    U = inv_A.solve(hessian)
+    x = U @ A.T @ b
+
     return x, U
 
 
 def solve_lu_colamd(A, b):
     # TODO: return x, U s.t. Ax = b, and Permutation_rows A Permutration_cols = LU with reordered LU decomposition.
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.splu.html
-    N = A.shape[1]
+    inv_A = splu(A, perm_spec = "NATURAL")
     x = np.zeros((N, ))
     U = eye(N)
     return x, U
 
 
 def solve_qr(A, b):
-    # TODO: return x, R s.t. Ax = b, and |Ax - b|^2 = |Rx - d|^2 + |e|^2
+    # Return x, R s.t. Ax = b, and |Ax - b|^2 = |Rx - d|^2 + |e|^2
     # https://github.com/theNded/PySPQR
+
+    # QR Factorization is more numerically stable, more than Cholesky
+    # It is pretty slow though :(
+    # This works directly on A, A = Q[R | 0]^T
+    # Q is an orthogonal basis, R is upper triangular
+
+    z, R, E, rank = rz(A, b)
     N = A.shape[1]
     x = np.zeros((N, ))
     R = eye(N)
@@ -61,7 +73,7 @@ def solve_qr_colamd(A, b):
 
 def solve(A, b, method='default'):
     '''
-    \param A (M, N) Jacobian matirx
+    \param A (M, N) Jacobian matrix
     \param b (M, 1) residual vector
     \return x (N, 1) state vector obtained by solving Ax = b.
     '''
